60062305e31bef6182e89e8b3fd368cf56e52d4f
Donna Galletta <donna.galletta@isobar.com>
2019-09-10 15:14:44 +1000

    Basic hello world

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from "./App"
 
-// JSX
+function App() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+
+
 ReactDOM.render(
    <App />, 
     document.getElementById("root")
