aea0a0ab7e069aeefaf6b6c1738916ec02307fcd
Donna Galletta <donna.galletta@isobar.com>
2019-09-10 17:47:04 +1000

    moving data into {{}} list instead

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ function App() {
   <div className="px-2 py-10">
     <div className="flex -mx-2">
       <ContactCard 
-        name="User 001"
-        imgUrl="https://images.unsplash.com/photo-1547038577-da80abbc4f19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-        phone="0401 0404 0404"
-        email="user001@demo.com"
-      />
-
-      <ContactCard 
-        name="User 002"
-        imgUrl="https://images.unsplash.com/photo-1560402974-0748b8635ac7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-        phone="0402 0404 0404"
-        email="user002@demo.com"
+        contact={{
+          name: "User 001", 
+          imgUrl: "https://images.unsplash.com/photo-1547038577-da80abbc4f19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", 
+          phone: "0401 111 111", 
+          email: "user001@demo.com"
+        }}
       />
 
      <ContactCard 
-        name="User 003"
-        imgUrl="https://images.unsplash.com/photo-1512749491228-caef5a7831d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-        phone="0403 0404 0404"
-        email="user003@demo.com"
+        contact={{
+          name: "User 002", 
+          imgUrl: "https://images.unsplash.com/photo-1560402974-0748b8635ac7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", 
+          phone: "0402 222 222", 
+          email: "user002@demo.com"
+        }}
+      />
+
+     <ContactCard 
+        contact={{
+          name: "User 003", 
+          imgUrl: "https://images.unsplash.com/photo-1512749491228-caef5a7831d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", 
+          phone: "0403 333 333", 
+          email: "user003@demo.com"
+        }}
       />
 
     </div>
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -5,10 +5,10 @@ function ContactCard(props) {
   return (
     <div className="w-1/3 px-2">
       <div className="bg-gray-200 p-5">
-        <img src={props.imgUrl} />
-        <h3>{props.name}</h3>
-        <p>Phone: {props.phone}</p>
-        <p>Email: {props.email}</p>
+        <img src={props.contact.imgUrl} />
+        <h3 className="text-3xl">{props.contact.name}</h3>
+        <p>Phone: {props.contact.phone}</p>
+        <p>Email: {props.contact.email}</p>
       </div>
     </div>
   );
