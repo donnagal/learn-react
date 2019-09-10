b54963ebbfc2dfced5483c943eed9100274341b9
Donna Galletta <donna.galletta@isobar.com>
2019-09-10 17:10:17 +1000

    small style updates

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ToDoItem from './ToDoItem';
 
 function App() {
   return (
-    <div className="container mx-auto m-10 py-10 bg-gray-800">
+    <div className="container mx-auto m-10 py-10 bg-gray-900">
       <ToDoItem />
       <ToDoItem />
       <ToDoItem />
