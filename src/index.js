f83920b36b038a746e586deb64d06c11ac82929c
Donna Galletta <donna.galletta@isobar.com>
2019-09-10 16:13:12 +1000

    add inline styles depending on time of day

--- src/index.js
+++ src/index.js
@@ -5,28 +5,31 @@ import ReactDOM from 'react-dom';
 function App() {
 
     const date = new Date()
+    // const date = new Date(2019, 12, 20, 17) //6 = 6AM
     const hours = date.getHours()
     let timeOfDay 
+    const styles = {
+        fontSize: 90
+    }
 
     if (hours < 12) {
         timeOfDay = "morning"
+        styles.color = "#FFC107"
+        styles.backgroundColor = "#FF5722"
     } else if (hours >= 12 && hours < 17) {
         timeOfDay = "afternoon"
+        styles.color = "#3F51B5"
+        styles.backgroundColor = "#2196F3"
     } else {
         timeOfDay = "night"
+        styles.color = "white"
+        styles.backgroundColor = "#607D8B"
     }
 
-    const styles = {
-        color: "#009688",
-        backgroundColor: "#27375d",
-        fontSize: 100,
-        padding: 200,
-    }
 
     return (
-      <div className="text-center m-32">
-        <h1 style={styles}>Good {timeOfDay}</h1>
-        <p style={{color: "#FF8C00", backgroundColor: "red", marginTop: 100}}>Sample of inline styles</p>
+      <div className="text-center">
+        <h1 style={styles} className="p-32">Good {timeOfDay}</h1>
       </div>
     );
   }
