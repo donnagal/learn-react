0e4300240c797c89269d1bd0e221bd5b2ea1c003
Donna Galletta <donna.galletta@isobar.com>
2019-09-11 09:47:33 +1000

    Adding a continual style with ternary operator

--- src/Joke.js
+++ src/Joke.js
@@ -5,8 +5,11 @@ function Joke(props) {
   return (
     <div className="w-full lg:w-1/5 px-2 mb-2">
       <div className="bg-gray-800 text-teal-300 p-5 rounded-lg">
-        <h3 className="text-2xl"><span className="text-white">Q: </span>{props.joke.question}</h3>
-        <p className="mt-5"><span className="text-white">A: </span>{props.joke.punchline}</p>
+      {/* style={{display: !props.joke.question && "none" }} */}
+        <h3 className="text-2xl mb-5" style={{display: props.joke.question ? "block" : "none" }}>
+          <span className="text-white">Q: </span>{props.joke.question}
+        </h3>
+        <p><span className="text-white">A: </span>{props.joke.punchline}</p>
       </div>
     </div>
   );
