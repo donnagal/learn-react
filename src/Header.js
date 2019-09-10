a6d3afd56a1eaad06243ce75f42b16f1379f8baf
Donna Galletta <donna.galletta@isobar.com>
2019-09-10 12:35:06 +1000

    add react site

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h3>Header</h3>
+      <ul>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo() {
+    return (
+     <div>
+         <h1>Donna Galletta</h1>
+         <p>A paragraph about self</p>
+         <ul>
+             <li>item</li>
+             <li>item</li>
+             <li>item</li>
+         </ul>
+     </div>
+    )
+}
+
+export default MyInfo
