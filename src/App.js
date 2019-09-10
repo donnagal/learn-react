df025b86293cebdc69f5f70e2a56192043c20e6e
Donna Galletta <donna.galletta@isobar.com>
2019-09-10 17:35:05 +1000

    Adding repeatable data in content

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import ContactCard from './ContactCard';
 
 function App() {
   return (
-<div className="px-2">
-  <div className="flex -mx-2">
-    <ContactCard />
-    <ContactCard />
-    <ContactCard />
+  <div className="px-2 py-10">
+    <div className="flex -mx-2">
+      <ContactCard 
+        name="User 001"
+        imgUrl="https://images.unsplash.com/photo-1547038577-da80abbc4f19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        phone="0401 0404 0404"
+        email="user001@demo.com"
+      />
+
+      <ContactCard 
+        name="User 002"
+        imgUrl="https://images.unsplash.com/photo-1560402974-0748b8635ac7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        phone="0402 0404 0404"
+        email="user002@demo.com"
+      />
+
+     <ContactCard 
+        name="User 003"
+        imgUrl="https://images.unsplash.com/photo-1512749491228-caef5a7831d7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        phone="0403 0404 0404"
+        email="user003@demo.com"
+      />
+
+    </div>
   </div>
-</div>
   );
 }
 
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 
-function ContactCard() {
+function ContactCard(props) {
   return (
     <div className="w-1/3 px-2">
-      <div className="bg-gray-400">
-        <img src="https://images.unsplash.com/photo-1568004374826-9d8377cbb660?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
-        <h3>Name</h3>
-        <p>info</p>
+      <div className="bg-gray-200 p-5">
+        <img src={props.imgUrl} />
+        <h3>{props.name}</h3>
+        <p>Phone: {props.phone}</p>
+        <p>Email: {props.email}</p>
       </div>
     </div>
   );
