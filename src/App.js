85023d72474fc46d36a2669e644ed57bf3fe1c1f
Donna Galletta <donna.galletta@isobar.com>
2019-09-11 15:09:03 +1000

    onClick show '1'

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ class App extends React.Component {
         this.state = {
             count: 0
         }
+        this.handledClick = this.handledClick.bind(this)
     }
 
     handledClick() {
-        console.log("Het meh")
+        this.setState({ count: 1})
     }
 
     render() {
         return (
             <div className="text-center mt-20">
-                <h1>{this.state.count}</h1>
+                <h1 className="text-6xl font-bold">{this.state.count}</h1>
                 
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
-                onClick={this.handledClick}> Change
+                    onClick={this.handledClick}> Change
                 </button>
             </div>
             )
