ce23c94581b8fbade9057a8f0ca413fe97730b66
Donna Galletta <donna.galletta@isobar.com>
2019-09-11 15:01:51 +1000

    Basic console log to trigger method click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 
-function handledMouseOver() {
-    console.log("Hoova at meh")
-}
 
-function handledClick() {
-    console.log("Het meh")
-}
+class App extends React.Component {
+    constructor() {
+        super ()
+        this.state = {
+            count: 0
+        }
+    }
 
-function App () {
-    return (
-        <div className="text-center mt-20">
-            <h1>Hello World</h1>
-            <img className="mx-auto p-5" src="https://www.fillmurray.com/200/100"
-            onMouseOver={handledMouseOver} />
-            
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
-                onClick={handledClick}>Button
-            </button>
-        </div>
-    )
-}
+    handledClick() {
+        console.log("Het meh")
+    }
+
+    render() {
+        return (
+            <div className="text-center mt-20">
+                <h1>{this.state.count}</h1>
+                
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+                onClick={this.handledClick}> Change
+                </button>
+            </div>
+            )
+        }
+    }
 
 export default App
