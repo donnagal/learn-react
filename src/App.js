d646a63ff9b2f6f997f7362510dbe088dd845c03
Donna Galletta <donna.galletta@isobar.com>
2019-09-11 11:42:44 +1000

    Add basic class App extends React.Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 
-function App () {
+class App extends React.Component {
+    render() {
     return (
-        <div>
-            <h1>Hello World</h1>
-    </div>
-    )
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
 }
 
 export default App
