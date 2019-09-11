fb6a428c39917a029682f7c3fd5ad2c557af78b3
Donna Galletta <donna.galletta@isobar.com>
2019-09-11 15:25:03 +1000

    adding a remove button

--- src/App.js
+++ src/App.js
@@ -8,12 +8,20 @@ class App extends React.Component {
             count: 0
         }
         this.handledClick = this.handledClick.bind(this)
+        this.handledClickRemoved = this.handledClickRemoved.bind(this)
     }
 
     handledClick() {
         this.setState(prevState => {
             return {
                 count: prevState.count +1
+            }
+        })
+    }
+    handledClickRemoved() {
+        this.setState(prevState => {
+            return {
+                count: prevState.count -1
             }
         })
     }
@@ -24,8 +32,16 @@ class App extends React.Component {
                 <h1 className="text-6xl font-bold">{this.state.count}</h1>
                 
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
-                    onClick={this.handledClick}> Change
+                    onClick={this.handledClick}> Add
                 </button>
+ 
+                <p className="mt-3">
+                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" 
+                        onClick={this.handledClickRemoved}> Remove
+                  </button>
+                </p>
+
+                {/* <ChildComponent count={this.state.count} /> */}
             </div>
             )
         }
