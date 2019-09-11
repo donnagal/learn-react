14fa35449c0b1f1877ec6c0c8f7bb64b4a048b8d
Donna Galletta <donna.galletta@isobar.com>
2019-09-11 14:43:25 +1000

    console log triggers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
+function handledMouseOver() {
+    console.log("Hoova at meh")
+}
+
+function handledClick() {
+    console.log("Het meh")
+}
 
 function App () {
     return (
-        <div>
+        <div className="text-center mt-20">
             <h1>Hello World</h1>
-    </div>
+            <img className="mx-auto p-5" src="https://www.fillmurray.com/200/100"
+            onMouseOver={handledMouseOver} />
+            
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+                onClick={handledClick}>Button
+            </button>
+        </div>
     )
 }
 
