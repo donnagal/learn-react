ffa9f2d7aa3dfd7e68d7565f1d4a4a1fef765a67
Donna Galletta <donna.galletta@isobar.com>
2019-09-11 11:40:54 +1000

    Set App file with basic hello world

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from "./App"
 
-function App() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-
-
+// JSX
 ReactDOM.render(
    <App />, 
     document.getElementById("root")
