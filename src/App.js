a0c0a8119ac65a0a4ed634af8b454260753fd164
Donna Galletta <donna.galletta@isobar.com>
2019-09-11 12:39:49 +1000

    demo 1

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import React from 'react';
 
-//#1
 class App extends React.Component {
+
+    constructor() {
+        super()
+        this.state = { answer: "Hell yeah" }
+    }
+
     render() {
     return (
             <div>
-                <Header username="Jane" />
-                <Greeting />
+                <h1>Is state important to know? {this.state.answer} </h1>
+                {/* <ChildComponemnt anwser={this.state.answer}/> */}
             </div>
         )
     }
 }
-
-//#2
-class Header extends React.Component {
-    render(props) {
-    return (
-            <header>
-               <p>Welcome, {this.props.username}!</p>
-            </header>
-        )
-    }
-}
-
-// #3
-class Greeting extends React.Component {
-    render() {
-        const date = new Date()
-        const hours = date.getHours()
-        let timeOfDay
-        
-        if (hours < 12) {
-            timeOfDay = "morning"
-        } else if (hours >= 12 && hours < 17) {
-            timeOfDay = "afternoon"
-        } else {
-            timeOfDay = "night"
-        }
-        return (
-            <h1>Good {timeOfDay} to you, sir or madam!</h1>
-        )
-    }
-}
-
 
 export default App
