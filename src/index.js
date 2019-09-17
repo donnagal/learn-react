712ae548e97ae98254deef36a241a7551f3a1d54
Donna Galletta <donna.galletta@isobar.com>
2019-09-17 15:24:51 +1000

    cont. on tut

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import React, { Component} from 'react';
+import React from 'react';
 import {render} from 'react-dom';
+
+let songList = [
+  { "title": "Fight for you", "artist": "Morgan Page", "time": 3.37,},
+  { "title": "You can have it All", "artist": "Zoo Brazil", "time": 5.36,},
+  { "title": "Call my name - Spencer Hill Remix", "artist": "Sultan + Shepard", "time": 6.30,},
+]
 
 const Song = ({title, artist, time}) => {
   return (
@@ -13,18 +19,24 @@ const Song = ({title, artist, time}) => {
 }
 
 //Parent Comp
-const Playlist = () => {
+const Playlist = ({songs}) => {
   return(
     <div>
-     <Song title="Fight for you" artist="Morgan Page" time="3.37"/>
-     <Song title="You can have it All" artist="Zoo Brazil" time="5.36"/>
-     <Song title="Call my name - Spencer Hill Remix" artist="Sultan + Shepard" time="6.30"/>
+        {songs.map(
+          (song, i)=> 
+        <Song 
+          key={i}
+          title={song.title} 
+          artist={song.artist} 
+          time={song.time}
+          />
+        )}
     </div>
   )
 }
 
 
 render(
-   <Playlist/>, 
+   <Playlist songs={songList}/>, 
     document.getElementById("root")
 )
