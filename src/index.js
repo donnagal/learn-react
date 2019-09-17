6f8d14ff2a24351f2073fc0f6381c0ee5945c49e
Donna Galletta <donna.galletta@isobar.com>
2019-09-17 14:31:56 +1000

    Adding props

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-function App() {
-    return (
+class Message extends React.Component {
+  render() {
+    return(
       <div>
-        <h1>Hello World</h1>
+        <h1 style={{color: this.props.color}}>
+          {this.props.msg}
+        </h1>
+        <p>I'll check back in {this.props.min} mintunes</p>
       </div>
-    );
+    )
   }
+}
 
 
 ReactDOM.render(
-   <App />, 
+   <Message color="blue" msg="How old are you?" min={5} />, 
     document.getElementById("root")
 )
