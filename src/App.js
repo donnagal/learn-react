8e3ebd726f97a40684fe7bde48ee3d25ab0e58ec
Donna Galletta <donna.galletta@isobar.com>
2019-09-23 15:28:04 +1000

    1. Installing styled comps. 2. Adapting based on props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import styled from 'styled-components'
 import Header from "./Header"
 import MainContent from "./MainContent"
 import Footer from "./Footer"
+
+const Button = styled.button`
+  font-size: 1.8rem;
+  padding: 0.5rem 1.5rem;
+  background: ${(props) => (props.primary ? 'red' : 'green')};
+  color: white;
+    &:hover {
+      opacity: .5;
+    }
+`;
+
+
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <Button> Button </Button>
+      <Button primary> primary Button </Button>
       <Footer />
+
     </div>
   );
 }
